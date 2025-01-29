30a3949dd19a2e34396544862da6a77ffd67d1eb
caverav <camilo@fvv.cl>
2025-01-29 20:12:33 -0300

    fix(hyprland): change keybinding for launching applications from SPACE to SHIFT+RETURN to avoid conflict with existing shortcuts

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -352,7 +352,7 @@
               "$CONTROL ALT, DELETE, exec, $term -e '${getExe pkgs.btop}'" # system monitor
 
               "$mainMod, A, exec, pkill -x rofi || $launcher" # launch desktop applications
-              "$mainMod, SPACE, exec, pkill -x rofi || $launcher" # launch desktop applications
+              "$mainMod SHIFT, RETURN, exec, pkill -x rofi || $launcher" # launch desktop applications
               "$mainMod, Z, exec, pkill -x rofi || $hyprScriptsDir/emoji.sh" # launch emoji picker
               #"$mainMod, tab, exec, pkill -x rofi || $hyprScriptsDir/rofilaunch.sh w" # switch between desktop applications
               # "$mainMod, R, exec, pkill -x rofi || $hyprScriptsDir/rofilaunch.sh f" # browse system files
